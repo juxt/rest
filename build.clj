2b01693d04f6b8208b54b7206583c14505b14fd5
Malcolm Sparks <mal@juxt.pro>
2020-12-09 05:53:22 +0000

    Extract dialects into config file

--- build.clj
+++ build.clj
@@ -2,8 +2,9 @@
 
 (ns juxt.rest
   (:require
-   [clojure.string :as str]
-   [clojure.java.shell :as sh]))
+   [clojure.edn :as edn]
+   [clojure.java.shell :as sh]
+   [clojure.string :as str]))
 
 (defn make-target-dir []
   (apply sh/sh ["mkdir" "-p" "target"]))
@@ -19,18 +20,25 @@
    (conj "-o" (format "target/README-%s.html" dialect))
    (conj "README.adoc")))
 
-(defn build []
+(defn build [{:keys [dialect includedir]}]
   (make-target-dir)
   (let [{:keys [exit out err]}
         (apply
          sh/sh
          (build-cmd
-          {:includedir "https://raw.githubusercontent.com/juxt/spin/master/doc"
-           :dialect "clojure"}))]
+          {:dialect dialect
+           :includedir includedir
+           }))]
 
     (when-not (str/blank? out) (println out))
     (when-not (str/blank? err) (println err))
 
     exit))
 
-(build)
+(def dialects (edn/read-string (slurp "dialects.edn")))
+
+(doseq [[dialect {:keys [includedir]}] dialects]
+  (println "Building dialect:" dialect)
+  (build
+   {:dialect dialect
+    :includedir includedir}))
